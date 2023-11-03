b4043ddddf8d710cf056e906be317e5511d248eb
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-11-03 09:15:01 -0300

    Enviando GET via url com a base url

--- agenda/config/process.php
+++ agenda/config/process.php
@@ -4,12 +4,30 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
-    
-    $query = "SELECT * FROM contacts";
+    $id;
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    }
 
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
+    //Retorna o dados de um contato
+    if(!empty($id)){
+        $query = "SELECT * FROM contacts WHERE id = :id";
 
-    $contacts = $stmt->fetchAll();
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+    }
+    else{
+        //Retorna todos os contatos
+        $contacts = [];
+        
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
 ?>
