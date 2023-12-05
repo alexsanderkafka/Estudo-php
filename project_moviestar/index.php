be67954e753a54049f5444dcb79b0921d363dea1
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-12-05 17:04:29 -0300

    Criando a estrutura do header, corpo do site e footer

--- project_moviestar/index.php
+++ project_moviestar/index.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MovieStar</title>
-    <link rel="short icon" href="img/moviestar.ico"/>
+    <link rel="short icon" href="<?=$BASE_URL?>img/moviestar.ico"/>
 
     <!-- Bootstrap-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.css" integrity="sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -24,8 +24,73 @@
 </head>
 <body>
 
-<h1>Testando o movieStar</h1>
-<i class="far fa-plus-square"></i>
+<header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+        <a href="<?=$BASE_URL?>" class="navbar-brand">
+            <img src="<?=$BASE_URL?>img/logo.svg" alt="MovieStar" id="logo">
+            <span id="moviestar-title">MovieStar</span>
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+
+        <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+            <button class="btn my-2 my-sm-0" type="submit">
+                <i class="fas fa-search"></i>
+            </button>
+        </form>
+
+        <div class="collapse navbar-collapse" id="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="<?=$BASE_URL?>auth.php" class="nav-link">Entrar / Cadastrar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div id="main-container" class="container-fluid">
+    <h1>Corpo do site</h1>
+</div>
+
+<footer id="footer">
+    <div class="social-container">
+        <ul>
+            <li>
+                <a href="###">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+            </li>
+            <li>
+                <a href="###">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </li>
+            <li>
+                <a href="###">
+                    <i class="fab fa-youtube"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div id="footer-links-container">
+        <ul>
+            <li>
+                <a href="#">Adicionar Filme</a>
+            </li>
+            <li>
+                <a href="#">Adicionar crítica</a>
+            </li>
+            <li>
+                <a href="#">Entrar / Registrar</a>
+            </li>
+        </ul>
+    </div>
+    <p>&copy; 2020 Salve galera</p>
+</footer>
 
 <!-- BOOTSTRAP JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
