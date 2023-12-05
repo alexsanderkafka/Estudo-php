5e690faf1307e7804f0e3462481cf2e8492bdc92
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-12-05 17:32:06 -0300

    Criando os estilos do footer, header e conteúdo do site

--- project_moviestar/index.php
+++ project_moviestar/index.php
@@ -35,12 +35,12 @@
             <i class="fas fa-bars"></i>
         </button>
 
-        <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-            <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-            <button class="btn my-2 my-sm-0" type="submit">
-                <i class="fas fa-search"></i>
-            </button>
-        </form>
+        <form action="<?= $BASE_URL ?>search.php" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+        <input type="text" name="q" id="search" class="form-control mr-sm-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        <button class="btn my-2 my-sm-0" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
 
         <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav">
@@ -57,7 +57,7 @@
 </div>
 
 <footer id="footer">
-    <div class="social-container">
+    <div id="social-container">
         <ul>
             <li>
                 <a href="###">
