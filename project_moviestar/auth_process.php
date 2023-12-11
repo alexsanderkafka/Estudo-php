6512799565c2d0da84bbe1ed03dea9cd0d9a5a35
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-12-11 11:33:47 -0300

    Sistema de cadastro

--- project_moviestar/auth_process.php
+++ project_moviestar/auth_process.php
@@ -8,6 +8,8 @@
 
     $message = new Message($BASE_URL);
 
+    $userDao = new UserDAO($conn, $BASE_URL);
+
     //Resgata o tipo do formulário
     $type = filter_input(INPUT_POST, "type");
 
@@ -21,11 +23,39 @@
 
         //Verificação de dados mínimos
         if($name && $lastname && $email && $password){
+            //Verifica se as senha batem
+            if($password === $confirmpassword){
+                //Verifica se o e-mail já está cadastrado no sistema
+                if($userDao->findByEmail($email) == false){
+                    $user = new User();
 
+                    //Criação de token e senha
+                    $userToken = $user->generateToken();
+                    $finalPassword = $user->generatePassword($password);
+
+                    $user->name = $name;
+                    $user->lastname = $lastname;
+                    $user->email = $email;
+                    $user->password = $finalPassword;
+                    $user->token = $userToken;
+
+                    $auth = true;
+
+                    $userDao->create($user, $auth);
+                }
+                else{
+                    //Enviar uma msg de erro, email já existe no sistema
+                    $message->setMessage("Usuário já cadastrado, tente outro e-mail.", "error", "back");    
+                }
+            }
+            else{
+                //Enviar uma msg de erro, senha não batem
+                $message->setMessage("As senhas não são iguais.", "error", "back");
+            }
         }
         else{
             //Enviar uma msg de erro, de dados faltantes
-            $message->setMessage("Por favor, preencha todos os campos", "error", "back");
+            $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
         }
     }
     else if($type === "login"){
--- project_moviestar/models/user.php
+++ project_moviestar/models/user.php
@@ -9,11 +9,18 @@
         public $bio;
         public $token;
 
+        public function generateToken(){
+             return bin2hex(random_bytes(50));
+        }
+
+        public function generatePassword($password){
+            return password_hash($password, PASSWORD_DEFAULT);
+        }
 
     }
 
     interface UserDAOInterface{
-        public function builUser($data);
+        public function buildUser($data);
         public function create(User $user, $authUser = false);
         public function update(User $user);
         public function verifyToken($protected = false);
